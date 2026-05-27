ccaf9df2d066aa65d0d93d348af8bc8abed37b8d
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-05-27 22:43:38 +0330

    improve security and fram

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
-const app = express()
+const app = express();
+
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
