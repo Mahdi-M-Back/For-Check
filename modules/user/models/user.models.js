85a7ce2362667a91cc92e21f6e9db4dde75f6491
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-05-28 23:39:02 +0330

    Create user model

---
+++ modules/user/models/user.models.js
@@ -0,0 +1,43 @@
+const mongoose =require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name']
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: {
+    type: String,
+    default:'default.jpg'
+  },
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user'
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false
+  },
+  passwordChangeAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
+});
+
+
+const User = mongoose.model('User', userSchema, 'Users');
+module.exports = User;
