35689ed362550d74518f85bd5fc94e83440d871e
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-05-30 23:48:37 +0330

    Create handler factory and add createOne function

---
+++ utilities/handlerfactory.js
@@ -0,0 +1,15 @@
+const APIFeatures = require('../utilities/apiFeatures');
+const AppError = require('../utilities/appError');
+const catchAsync = require('../utilities/catchAsync');
+
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
