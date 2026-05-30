b0d0dac0baf053fa5ee33e398526e2f1cab2a7e3
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-05-30 23:52:15 +0330

    Add getOne in handler factory

--- utilities/handlerfactory.js
+++ utilities/handlerfactory.js
@@ -2,19 +2,19 @@ const APIFeatures = require('../utilities/apiFeatures');
 const AppError = require('../utilities/appError');
 const catchAsync = require('../utilities/catchAsync');
 
-exports.createOne = Model =>
+exports.createOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.create(req.body);
 
     res.status(201).json({
       status: 'success',
       data: {
-        data: doc
-      }
+        data: doc,
+      },
     });
   });
 
-  exports.getAll = Model =>
+exports.getAll = (Model) =>
   catchAsync(async (req, res, next) => {
     // To allow for nested GET reviews on tour (hack)
     let filter = {};
@@ -34,7 +34,25 @@ exports.createOne = Model =>
       status: 'success',
       results: doc.length,
       data: {
-        doc
-      }
+        doc,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        doc,
+      },
     });
   });
