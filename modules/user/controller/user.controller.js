8ac663a3ccae7c65d29cad15c80e64a791ca37fd
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-01 23:37:26 +0330

    Add getUser info by yourself and createUser by admin or another role.

--- modules/user/controller/user.controller.js
+++ modules/user/controller/user.controller.js
@@ -1,9 +1,16 @@
 const User = require('../models/user.models')
 const factory = require('../../../utilities/handlerfactory')
 
-
+// These function not for user for another role like admin owner, ...
+exports.createUser = factory.createOne(Review);
 exports.getAllUser = factory.getAll(User)
 exports.getOneUser = factory.getOne(User)
 exports.deleteUser = factory.deleteOne(User)
 //Don't update password with this 
 exports.updateUser = factory.updateOne(User)
+
+// These function exactully for user
+
+exports.getMe = (req,res,next)=>{
+  req.params.id = req.user.id
+}
