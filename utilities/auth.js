7b9c9e022e4880438198126a2a2f87ee379c1070
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-02 23:49:16 +0330

    add create token for all function we need

--- utilities/auth.js
+++ utilities/auth.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 
 
-const createToken = (id) => {
+const signToken = (id) => {
   return {
     accessToken: jwt.sign(
       { id },
@@ -15,4 +15,25 @@ const createToken = (id) => {
       process.env.REFRESH_TOKEN_EXPIRES_TIME,
     ),
   };
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
+  return sendResponse({
+    res,
+    statusCode,
+    status: true,
+    data:user
+  });
 };
