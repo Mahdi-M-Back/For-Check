4faa6865a7395765204a123ed2e2cc210170615e
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-02 23:58:28 +0330

    add login

--- utilities/auth.js
+++ utilities/auth.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const catchAsync = require('./catchAsync');
 
 
 const signToken = (id) => {
@@ -49,4 +50,22 @@ exports.signup = catchAsync(async (req, res, next) => {
   // // console.log(url);
   // new Email(newUser, url).sendWelcome();
   createSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if user exists
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3) If everything ok, send token to cleint
+  createSendToken(user, 200, res);
 });
