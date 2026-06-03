bdbfaa5ae0fde22a337bf67bb1283f526da458ac
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-03 22:07:42 +0330

    .

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
 const globalErrorHandler = require('./utilities/errorHandler')
 
 const app = express();
@@ -30,13 +28,6 @@ app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
-
-// Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
-
-// Data sanitization against XSS
-app.use(xss());
-
 
 
 app.use(globalErrorHandler);
