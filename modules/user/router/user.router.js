d99cfa8d3823aa75a945416f33190e7e2b0e8fe2
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-04 18:42:42 +0330

    .

---
+++ modules/user/router/user.router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const userController = require('../controller/user.controller');
+const userMiddleware = require('../middleware/user.middleware');
+const authController = require('../../../utilities/auth');
+
+const router = express.Router();
+
+router.post('/signup',userMiddleware.signup,userController.signup);
+router.post('/login',userMiddleware.login,userController.login);
+
+router.use(userMiddleware.protect);
+
+router
+  .route('/me')
+  .get(userController.getMe, userController.getOneUser)
+  // .patch(userController);
+
+router
+  .route('/')
+  .get(userController.getAllUser)
+  .post(userController.createUser);
+router
+  .route('/:id')
+  .get(userController.getOneUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
