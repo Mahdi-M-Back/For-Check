665f03c611bce4202bbbc006041138b730f70362
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-06 16:00:48 +0330

    add route product file and create route.

---
+++ modules/product/router/product.route.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router();
+
+const productController = require('./../controller/product.controller')
+
+
+router.route('/').post(productController.create)
+
+module.exports = router
