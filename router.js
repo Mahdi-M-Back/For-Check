5ec0c9733e6362ad7c9f4b2c9952991316580f30
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-06 16:00:19 +0330

    add product

--- router.js
+++ router.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router()
 
 const userRouter = require('./modules/user/router/user.router')
+const productRouter = require('./modules/product/router/product.route')
+
 
 router.use('/users',userRouter)
+router.use('/products', productRouter)
 
 module.exports = router; 
