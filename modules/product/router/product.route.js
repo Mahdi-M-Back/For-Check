21f359c699aca956020b7a24630ee614a4e57c96
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-07 11:07:36 +0330

    add middleware to route.

--- modules/product/router/product.route.js
+++ modules/product/router/product.route.js
@@ -6,5 +6,5 @@ const productMiddleware = require('./../middleware/product.middleware')
 
 
 router.route('/').post(productMiddleware.create,productController.create).get(productController.getAll)
-router.route('/:id').get(productController.getOne).patch(productController.update).delete(productController.delete)
+router.route('/:id').get(productController.getOne).patch(productMiddleware.update,productController.update).delete(productController.delete)
 module.exports = router
