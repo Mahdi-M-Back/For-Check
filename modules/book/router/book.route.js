bb5b425541d62013c06dd4a1ee01c9805ddc2bd1
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-07 14:19:12 +0330

    add get all route.

--- modules/book/router/book.route.js
+++ modules/book/router/book.route.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const bookController = require('./../controller/book.controller')
-const {protect} = require('./../../user/middleware/user.middleware')
+const express = require('express');
+const bookController = require('./../controller/book.controller');
+const { protect } = require('./../../user/middleware/user.middleware');
 
-const router =express.Router()
-router.use(protect)
+const router = express.Router();
 
-router.post('/',bookController.create)
+router.use(protect);
 
-module.exports = router
+router.route('/').post(bookController.create).get(bookController.getAll);
+
+module.exports = router;
