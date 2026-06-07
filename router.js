647ee85819bdde7bae3e00204a15c6a89291427c
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-07 13:51:51 +0330

    add bookroute

--- router.js
+++ router.js
@@ -3,9 +3,11 @@ const router = express.Router()
 
 const userRouter = require('./modules/user/router/user.router')
 const productRouter = require('./modules/product/router/product.route')
+const bookRouter = require('./modules/book/router/book.route')
 
 
 router.use('/users',userRouter)
 router.use('/products', productRouter)
+router.use('/books', bookRouter)
 
 module.exports = router; 
