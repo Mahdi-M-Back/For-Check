c8a906dc4ffd98e872ce30d73296652cc61ba9f1
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-07 11:24:55 +0330

    .

--- modules/product/router/product.route.js
+++ modules/product/router/product.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const productController = require('./../controller/product.controller')
 const productMiddleware = require('./../middleware/product.middleware')
 
-
 router.route('/').post(productMiddleware.create,productController.create).get(productController.getAll)
 router.route('/:id').get(productController.getOne).patch(productMiddleware.update,productController.update).delete(productController.delete)
+
 module.exports = router
