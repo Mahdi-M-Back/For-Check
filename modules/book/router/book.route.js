0f349174406065699d5e971e3d98812ae441eef0
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-08 13:21:47 +0330

    add create middleware.

--- modules/book/router/book.route.js
+++ modules/book/router/book.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bookController = require('./../controller/book.controller');
+const bookMiddleware = require('./../middleware/book.middleware');
 const { protect,restrictTo } = require('./../../user/middleware/user.middleware');
 
 const router = express.Router();
 
 router.use(protect);
 
-router.post('/',bookController.create)
+router.post('/',bookMiddleware.create,bookController.create)
 router.get('/:id',bookController.getOne);
 
 router.use(restrictTo('admin','owner'));
