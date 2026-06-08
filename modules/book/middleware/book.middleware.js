fe7ab493bae71e216087e9e2705e64d125ae8bcf
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-08 13:00:32 +0330

    add create middleware.

---
+++ modules/book/middleware/book.middleware.js
@@ -0,0 +1,60 @@
+const Validator = require('./../../../utilities/Validator')
+const sendResponse = require('./../../../utilities/Response')
+
+exports.create = (req, res, next) => {
+  const { product, price } = req.body;
+
+  // price
+  const priceDefinedCheck = Validator.isDefined(price);
+  if (!priceDefinedCheck.success) {
+    return sendResponse({
+      res,
+      statusCode: 400,
+      enMessage: priceDefinedCheck.enMessage,
+      data: 'price',
+    });
+  }
+
+  const priceNumberCheck = Validator.isNumber(price);
+  if (!priceNumberCheck.success) {
+    return sendResponse({
+      res,
+      statusCode: 400,
+      enMessage: priceNumberCheck.enMessage,
+      data: 'price',
+    });
+  }
+
+  // product
+  const productDefinedCheck = Validator.isDefined(product);
+  if (!productDefinedCheck.success) {
+    return sendResponse({
+      res,
+      statusCode: 400,
+      enMessage: productDefinedCheck.enMessage,
+      data: 'product',
+    });
+  }
+
+  const productEmptyCheck = Validator.isNotEmpty(product);
+  if (!productEmptyCheck.success) {
+    return sendResponse({
+      res,
+      statusCode: 400,
+      enMessage: productEmptyCheck.enMessage,
+      data: 'product',
+    });
+  }
+
+  const productIdCheck = Validator.isMongoId(product);
+  if (!productIdCheck.success) {
+    return sendResponse({
+      res,
+      statusCode: 400,
+      enMessage: productIdCheck.enMessage,
+      data: 'product',
+    });
+  }
+
+  next();
+};
