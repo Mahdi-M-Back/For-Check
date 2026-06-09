3dadfff3d32deb9507b1f4648530d282d30aab8a
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-09 17:15:22 +0330

    add reset password url.

--- modules/user/router/user.router.js
+++ modules/user/router/user.router.js
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post('/signup', userMiddleware.signup, userController.signup);
 router.post('/login', userMiddleware.login, userController.login);
 router.post('/forgotPassword',userController.forgotPassword)
+router.patch('/resetPassword/:token',userController.resetPassword)
+
 router.use(userMiddleware.protect);
 
 router
