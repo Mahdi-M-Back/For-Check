deb86a70d0554326fa4560ff992b62eff413f4c5
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-10 22:50:13 +0330

    add update review

--- modules/review/controller/review.controller.js
+++ modules/review/controller/review.controller.js
@@ -20,6 +20,33 @@ exports.create = catchAsync(async (req, res) => {
     statusCode: 201,
     success: true,
     enMessage: 'Review submit succeessfully.!',
+    data:newReview
   });
 });
 
+exports.update = catchAsync(async (req, res) => {
+  const productId = req.params.id;
+  const findReview = await Review.findById(productId);
+
+  if (!findReview) {
+    return sendResponse({
+      res,
+      statusCode: 400,
+      success: false,
+      enMessage: 'Review not found.!',
+    });
+  }
+
+  const limitField = filterObj(req.body, 'review', 'rating', 'product');
+  const updateReview = await Review.findByIdAndUpdate(productId, limitField, {
+    new: true,
+  });
+
+  return sendResponse({
+    res,
+    statusCode: 200,
+    success: true,
+    enMessage: 'Review updated successfully.',
+    data:updateReview
+  });
+});
