3fcf2f8f559b8a4c5a1990366f4355c2bd871329
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-11 22:25:56 +0330

    add permission.

--- modules/review/router/review.route.js
+++ modules/review/router/review.route.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const reviewController = require("../controller/review.controller");
 const reviewMiddleware = require("../middleware/review.middleware");
+const { protect ,restrictTo } = require('./../../user/middleware/user.middleware')
 
 const router = express.Router();
+
+router.use(protect)
 
 router.route("/")
   .post(reviewMiddleware.create, reviewController.create)
@@ -12,6 +15,7 @@ router.route("/:id")
   .get(reviewController.getOne)
   .delete(reviewMiddleware.delete, reviewController.delete);
 
+router.use(restrictTo('admin','owner'))
 router.patch("/:id", reviewMiddleware.update, reviewController.update);
 
 module.exports = router;
