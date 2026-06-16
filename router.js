367aacc392d44f888acd319ff81c0a06e4bdbdf5
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-16 19:45:02 +0330

    add review route

--- router.js
+++ router.js
@@ -4,10 +4,12 @@ const router = express.Router()
 const userRouter = require('./modules/user/router/user.router')
 const productRouter = require('./modules/product/router/product.route')
 const bookRouter = require('./modules/book/router/book.route')
+const reviewRouter = require('./modules/review/router/review.route')
 
 
 router.use('/users',userRouter)
 router.use('/products', productRouter)
 router.use('/books', bookRouter)
+router.use('/reviews', reviewRouter )
 
 module.exports = router; 
