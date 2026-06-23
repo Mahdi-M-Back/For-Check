b2447b5694c08f2973b232e56890d12980dbcb7b
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-23 23:30:41 +0330

    feat: add filterObj utility function for object field filtering

---
+++ src/utilities/filterObj.js
@@ -0,0 +1,7 @@
+const filterObj = (obj, ...allowedFields) =>
+  Object.keys(obj).reduce((acc, key) => {
+    if (allowedFields.includes(key)) acc[key] = obj[key];
+    return acc;
+  }, {});
+
+module.exports = filterObj;
