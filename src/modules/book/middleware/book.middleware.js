dafdc932f33565775ef1c00ca7da149480a2f114
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-24 12:32:33 +0330

    refactor: replace manual validation with validateBody utility for create and update methods

--- src/modules/book/middleware/book.middleware.js
+++ src/modules/book/middleware/book.middleware.js
@@ -1,68 +1,16 @@
-const Validator = require('./../../../utilities/Validator');
-const sendResponse = require('./../../../utilities/Response');
+const {
+  validateBody,
+  required,
+  mongoId,
+  number,
+  optional,
+} = require('../../../utilities/validateBody');
 
-exports.create = (req, res, next) => {
-  const { product } = req.body;
+exports.create = validateBody({
+  product: [required, mongoId],
+});
 
-  const productDefinedCheck = Validator.isDefined(product);
-  if (!productDefinedCheck.success) {
-    return sendResponse({
-      res,
-      statusCode: 400,
-      enMessage: productDefinedCheck.enMessage,
-      data: 'product',
-    });
-  }
-
-  const productEmptyCheck = Validator.isNotEmpty(product);
-  if (!productEmptyCheck.success) {
-    return sendResponse({
-      res,
-      statusCode: 400,
-      enMessage: productEmptyCheck.enMessage,
-      data: 'product',
-    });
-  }
-
-  const productIdCheck = Validator.isMongoId(product);
-  if (!productIdCheck.success) {
-    return sendResponse({
-      res,
-      statusCode: 400,
-      enMessage: productIdCheck.enMessage,
-      data: 'product',
-    });
-  }
-
-  next();
-};
-
-exports.update = (req, res, next) => {
-  const { product, price } = req.body;
-
-  if (price !== undefined) {
-    const priceNumberCheck = Validator.isNumber(price);
-    if (!priceNumberCheck.success) {
-      return sendResponse({
-        res,
-        statusCode: 400,
-        enMessage: priceNumberCheck.enMessage,
-        data: 'price',
-      });
-    }
-  }
-
-  if (product !== undefined) {
-    const productIdCheck = Validator.isMongoId(product);
-    if (!productIdCheck.success) {
-      return sendResponse({
-        res,
-        statusCode: 400,
-        enMessage: productIdCheck.enMessage,
-        data: 'product',
-      });
-    }
-  }
-
-  next();
-};
+exports.update = validateBody({
+  product: optional(mongoId),
+  price:   optional(number),
+});
