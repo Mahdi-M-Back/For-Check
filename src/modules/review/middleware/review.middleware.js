c98053f82a1e2a0e2d615f8f9a8ee06546ea4776
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-24 12:33:05 +0330

    refactor: simplify review validation by utilizing validateBody utility

--- src/modules/review/middleware/review.middleware.js
+++ src/modules/review/middleware/review.middleware.js
@@ -1,129 +1,20 @@
-const catchAsync = require('./../../../utilities/catchAsync');
-const Review = require('../model/review.model');
-const sendResponse = require('./../../../utilities/Response');
-const Validator = require('./../../../utilities/Validator');
+const {
+  validateBody,
+  required,
+  string,
+  notEmpty,
+  number,
+  mongoId,
+  optional,
+} = require('../../../utilities/validateBody');
 
-exports.create = (req, res, next) => {
-  const { review, product, rating } = req.body;
+exports.create = validateBody({
+  product: [required, mongoId],
+  review:  [required, string, notEmpty],
+  rating:  [required, number],
+});
 
-  // product
-  const productDefinedCheck = Validator.isDefined(product);
-  if (!productDefinedCheck.success) {
-    return sendResponse({
-      res,
-      statusCode: 400,
-      enMessage: productDefinedCheck.enMessage,
-      data: 'product',
-    });
-  }
-  const productIdCheck = Validator.isMongoId(product);
-  if (!productIdCheck.success) {
-    return sendResponse({
-      res,
-      statusCode: 400,
-      enMessage: productIdCheck.enMessage,
-      data: 'product',
-    });
-  }
-
-  // review
-  const reviewDefinedCheck = Validator.isDefined(review);
-  if (!reviewDefinedCheck.success) {
-    return sendResponse({
-      res,
-      statusCode: 400,
-      enMessage: reviewDefinedCheck.enMessage,
-      data: 'review',
-    });
-  }
-  const reviewEmptyCheck = Validator.isNotEmpty(review);
-  if (!reviewEmptyCheck.success) {
-    return sendResponse({
-      res,
-      statusCode: 400,
-      enMessage: reviewEmptyCheck.enMessage,
-      data: 'review',
-    });
-  }
-  const reviewStringCheck = Validator.isString(review);
-  if (!reviewStringCheck.success) {
-    return sendResponse({
-      res,
-      statusCode: 400,
-      enMessage: reviewStringCheck.enMessage,
-      data: 'review',
-    });
-  }
-
-  // rating
-  const ratingDefinedCheck = Validator.isDefined(rating);
-  if (!ratingDefinedCheck.success) {
-    return sendResponse({
-      res,
-      statusCode: 400,
-      enMessage: ratingDefinedCheck.enMessage,
-      data: 'rating',
-    });
-  }
-  const ratingNumberCheck = Validator.isNumber(rating);
-  if (!ratingNumberCheck.success) {
-    return sendResponse({
-      res,
-      statusCode: 400,
-      enMessage: ratingNumberCheck.enMessage,
-      data: 'rating',
-    });
-  }
-
-  next();
-};
-
-exports.update = (req, res, next) => {
-  const { review, product, rating } = req.body;
-  if (product !== undefined) {
-    const productIdCheck = Validator.isMongoId(product);
-    if (!productIdCheck.success) {
-      return sendResponse({
-        res,
-        statusCode: 400,
-        enMessage: productIdCheck.enMessage,
-        data: 'product',
-      });
-    }
-  }
-
-  if (review !== undefined) {
-    const reviewStringCheck = Validator.isString(review);
-    if (!reviewStringCheck.success) {
-      return sendResponse({
-        res,
-        statusCode: 400,
-        enMessage: reviewStringCheck.enMessage,
-        data: 'review',
-      });
-    }
-    const reviewEmptyCheck = Validator.isNotEmpty(review);
-    if (!reviewEmptyCheck.success) {
-      return sendResponse({
-        res,
-        statusCode: 400,
-        enMessage: reviewEmptyCheck.enMessage,
-        data: 'review',
-      });
-    }
-  }
-
-  if (rating !== undefined) {
-    const ratingNumberCheck = Validator.isNumber(rating);
-    if (!ratingNumberCheck.success) {
-      return sendResponse({
-        res,
-        statusCode: 400,
-        enMessage: ratingNumberCheck.enMessage,
-        data: 'rating',
-      });
-    }
-  }
-
-  next();
-};
+exports.update = validateBody({
+  review: optional(string, notEmpty),
+  rating: optional(number),
+});
