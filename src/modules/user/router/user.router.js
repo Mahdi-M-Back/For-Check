81b68b42c4d60a146420e80ffc848a5983ac19a7
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-27 22:08:07 +0330

    refactor: clean up user router and add refresh endpoint

--- src/modules/user/router/user.router.js
+++ src/modules/user/router/user.router.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const userController = require('../controller/user.controller');
 const adminController = require('../controller/admin.controller');
+const refreshController = require('../controller/refresh.controller');
 const userMiddleware = require('../middleware/user.middleware');
 
 const router = express.Router();
 
 router.post('/signup', userMiddleware.signup, userController.signup);
 router.post('/login', userMiddleware.login, userController.login);
-router.post('/forgotPassword',userMiddleware.forgotPassword,userController.forgotPassword)
-router.patch('/resetPassword/:token',userMiddleware.resetPassword,userController.resetPassword)
+router.post('/forgotPassword', userMiddleware.forgotPassword, userController.forgotPassword);
+router.patch('/resetPassword/:token', userMiddleware.resetPassword, userController.resetPassword);
+
+router.post('/refresh', refreshController.refresh);
 
 router.use(userMiddleware.protect);
 
-router.patch('/updatePassword',userMiddleware.resetPassword,userController.updatePassword)
+router.patch('/updatePassword', userMiddleware.updatePassword, userController.updatePassword);
 router
   .route('/me')
   .get(userController.getMe)
@@ -27,14 +30,9 @@ router.patch(
 );
 
 router.use(userMiddleware.restrictTo('admin', 'owner'));
-
 router.route('/').get(adminController.getAll);
-// .post(userController.createUser);
-
-router
-  .route('/:id')
+router.route('/:id')
   .get(adminController.getOne)
-  // HARD DELETE
   .delete(adminController.delete);
 
 module.exports = router;
