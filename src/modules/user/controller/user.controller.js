bf3f973f04b1593d66d0897c9205ddc9f7b1975b
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-27 22:06:45 +0330

    refactor: streamline user controller methods and improve response messages

--- src/modules/user/controller/user.controller.js
+++ src/modules/user/controller/user.controller.js
@@ -1,5 +1,4 @@
 const User = require('./../model/user.models');
-const factory = require('../../../utilities/handlerfactory');
 const AppError = require('../../../utilities/appError');
 const sendResponse = require('./../../../utilities/Response');
 const Email = require('./../../../utilities/email');
@@ -7,37 +6,18 @@ const catchAsync = require('./../../../utilities/catchAsync');
 const { createSendToken } = require('./../../../utilities/auth');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
-
-const filterObj = (obj, ...allowedFields) => {
-  const newObj = {};
-  Object.keys(obj).forEach((el) => {
-    if (allowedFields.includes(el)) newObj[el] = obj[el];
-  });
-  return newObj;
-};
+const filterObj = require('../../../utilities/filterObj');
 
 exports.getMe = catchAsync(async (req, res) => {
-  const user = await User.findById(req.user.id);
-
-  if (!user) {
-    return sendResponse({
-      res,
-      statusCode: 401,
-      success: false,
-      enMessage: 'User not found.!',
-    });
-  }
-
   return sendResponse({
     res,
     statusCode: 200,
     success: true,
-    data: user,
+    data: req.user,
   });
 });
 
 exports.updateMe = catchAsync(async (req, res) => {
-  const user = req.user;
   const filteredBody = filterObj(req.body, 'name', 'userName');
 
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
@@ -55,11 +35,11 @@ exports.updateMe = catchAsync(async (req, res) => {
 
 exports.deleteMe = catchAsync(async (req, res) => {
   await User.findByIdAndUpdate(req.user.id, { isDeleted: true });
+
   return sendResponse({
     res,
-    statusCode: 204,
+    statusCode: 200,
     success: true,
-    message: 'کاربر حذف شد',
     enMessage: 'User deleted successfully.',
     data: null,
   });
@@ -70,35 +50,36 @@ exports.signup = catchAsync(async (req, res) => {
   if (user) {
     return sendResponse({
       res,
-      statusCode: 401,
+      statusCode: 409,
       success: false,
-      enMessage: 'This email used by another user.',
+      enMessage: 'This email is already in use.',
     });
   }
 
-  const salt = await bcrypt.genSaltSync(12);
-  const hashPassword = await bcrypt.hashSync(req.body.password, salt);
+  const salt = await bcrypt.genSalt(12);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
   const filteredBody = filterObj(req.body, 'name', 'userName', 'email');
+
   const newUser = await User.create({
     ...filteredBody,
     password: hashPassword,
     role: 'user',
   });
-  await new Email(newUser, "").send('welcome', 'Welcome to My Test Project..!');
+
+  await new Email(newUser, '').send('welcome', 'Welcome to My Test Project!');
   createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res) => {
   const { email, password } = req.body;
-  // Check if user exists && password is correct
-  const user = await User.findOne({ email }).select('+password');
 
+  const user = await User.findOne({ email }).select('+password');
   if (!user || !(await bcrypt.compare(password, user.password))) {
     return sendResponse({
       res,
       statusCode: 401,
       success: false,
-      enMessage: 'Incorrect email or password',
+      enMessage: 'Incorrect email or password.',
     });
   }
 
@@ -108,12 +89,13 @@ exports.login = catchAsync(async (req, res) => {
 exports.forgotPassword = catchAsync(async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
+
   if (!user) {
     return sendResponse({
       res,
-      statusCode: 404,
-      success: false,
-      enMessage: 'If this email exists, a reset link was sent.',
+      statusCode: 200,
+      success: true,
+      enMessage: 'If this email is registered, a reset link has been sent.',
     });
   }
 
@@ -122,35 +104,31 @@ exports.forgotPassword = catchAsync(async (req, res) => {
     .createHash('sha256')
     .update(resetToken)
     .digest('hex');
-  user.passwordResetExpires = Date.now() + 15 * 60 * 1000; // 15 minute
+  user.passwordResetExpires = Date.now() + 15 * 60 * 1000;
 
   try {
-    const resetURL = `${req.protocol}://${req.get(
-      'host',
-    )}/api/v1/users/resetPassword/${resetToken}`;
-    console.log(resetURL);
-    const enMessage = `Forgot your password? Submit a PATCH request 
-      with new password and passwordConfirm to:${resetURL}.
-      \nIf you didn't forgot your password, please ignore this email!`;
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
 
+    const enMessage = `Forgot your password? Submit a PATCH request with your new password to: ${resetURL}.\nIf you didn't request this, please ignore this email.`;
     await new Email(user, resetURL).send('Password Reset', enMessage);
-
     await user.save({ validateBeforeSave: false });
+
     return sendResponse({
       res,
       statusCode: 200,
-      enMessage: 'Token sent to email.!',
       success: true,
+      enMessage: 'If this email is registered, a reset link has been sent.',
     });
   } catch (err) {
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
     await user.save({ validateBeforeSave: false });
+
     return sendResponse({
       res,
-      statusCode: 401,
+      statusCode: 500,
       success: false,
-      enMessage: 'There was an error sending the email. Try again later!',
+      enMessage: 'There was an error sending the email. Try again later.',
     });
   }
 });
@@ -170,36 +148,38 @@ exports.resetPassword = catchAsync(async (req, res) => {
   if (!user) {
     return sendResponse({
       res,
-      statusCode: 404,
+      statusCode: 400,
       success: false,
-      enMessage: 'Somethings went very wrong.!',
+      enMessage: 'Token is invalid or has expired.',
     });
   }
 
-  const salt = await bcrypt.genSaltSync(12);
-  const hashPassword = await bcrypt.hashSync(password, salt);
-  user.password = hashPassword;
+  const salt = await bcrypt.genSalt(12);
+  user.password = await bcrypt.hash(password, salt);
   user.passwordChangeAt = Date.now();
   user.passwordResetToken = undefined;
   user.passwordResetExpires = undefined;
 
   await user.save();
-
   createSendToken(user, 200, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res) => {
-  const { password } = req.body;
-  const user = await User.findById(req.user.id);
-  const salt = await bcrypt.genSaltSync(12);
-  const hashPassword = await bcrypt.hashSync(password, salt);
-  user.password = hashPassword;
-  await user.save();
+  const { currentPassword, newPassword } = req.body;
+  const user = await User.findById(req.user.id).select('+password');
 
-  return sendResponse({
-    res,
-    statusCode: 200,
-    success: true,
-    enMessage: 'Password reset successfully.',
-  });
+  if (!user || !(await bcrypt.compare(currentPassword, user.password))) {
+    return sendResponse({
+      res,
+      statusCode: 401,
+      success: false,
+      enMessage: 'Current password is wrong.',
+    });
+  }
+
+  const salt = await bcrypt.genSalt(12);
+  user.password = await bcrypt.hash(newPassword, salt);
+  user.passwordChangeAt = Date.now() - 1000;
+  await user.save();
+  createSendToken(user, 200, res);
 });
