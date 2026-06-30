5823b070c558e29cb56d1b76d27b7068461a3830
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-30 23:32:22 +0330

    refactor: improve code formatting and consistency in review route

--- src/modules/review/router/review.route.js
+++ src/modules/review/router/review.route.js
@@ -1,23 +1,24 @@
-const express = require("express");
-const reviewController = require("../controller/review.controller");
-const reviewMiddleware = require("../middleware/review.middleware");
-const { protect ,restrictTo } = require('./../../user/middleware/user.middleware')
+const express = require('express');
+const reviewController = require('../controller/review.controller');
+const reviewMiddleware = require('../middleware/review.middleware');
+const {
+  protect,
+  restrictTo,
+} = require('./../../user/middleware/user.middleware');
 
 const router = express.Router();
 
 router.get('/', reviewController.getAll);
 router.get('/:id', reviewController.getOne);
 
-router.use(protect)
+router.use(protect);
 
-router.route("/")
-  .post(reviewMiddleware.create, reviewController.create)
+router.route('/').post(reviewMiddleware.create, reviewController.create);
 
-
-router.route("/:id")
-  // .delete(reviewMiddleware.delete, reviewController.delete);
-
-router.use(restrictTo('admin','owner'))
-router.patch("/:id", reviewMiddleware.update, reviewController.update);
+router.use(restrictTo('admin', 'owner'));
+router
+  .route('/:id')
+  .patch(reviewMiddleware.update, reviewController.update)
+  .delete(reviewController.delete);
 
 module.exports = router;
